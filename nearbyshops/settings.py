5a9f6224fcafb16a9667bceb1f38e18b8a01fa0f
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-07-12 23:12:45 +0000

    Update settings.py

--- nearbyshops/settings.py
+++ nearbyshops/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
-django_on_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
