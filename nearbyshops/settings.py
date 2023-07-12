8eb21b61fb2bf4eb71bfb990c84e6f9d3ec20524
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-07-12 23:10:06 +0000

    Update settings.py

--- nearbyshops/settings.py
+++ nearbyshops/settings.py
@@ -10,10 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path,os
+from pathlib import Path
 import os
-import django_on_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
